edc6004076b9cf21359b8b4461d27f4268e08818
BloodGhost <1124109099@qq.com>
2017-01-19 00:15:21 +0800

    Add files via upload

---
+++ adding1.py
@@ -0,0 +1,6 @@
+print('\n'.join([''.join([('Happy'[(x-y) % len('happy')] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+
+print('\n'.join([''.join([('New'[(x-y) % len('New')] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+
+print('\n'.join([''.join([('Year'[(x-y) % len('Year')] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+
